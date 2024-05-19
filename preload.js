70dd23943e99cbc0ee86d91e2cda722f6d89b87d
kumshing-wilson-huang <165102973+kumshing-wilson-huang@users.noreply.github.com>
2024-05-19 10:29:14 +0800

    init

--- preload.js
+++ preload.js
@@ -1,2 +1,21 @@
-// 在preload.js文件中编写预加载代码（如果需要，可以为空文件）：
-console.log("Preload script loaded");
+/**
+ * 在preload.js文件中编写预加载代码（如果需要，可以为空文件）：
+ */
+const { contextBridge, ipcRenderer } = require('electron');
+
+console.log("Preload script loaded.");
+
+contextBridge.exposeInMainWorld('electron', {
+    //require: require,
+    requestLocale: () => {
+        // console.log("Requesting locale...");
+        ipcRenderer.send('request-locale');
+    },
+    onSetLocale: (callback) => {
+        // console.log("Setting up locale listener...");
+        ipcRenderer.on('set-locale', (event, localeData) => {
+            // console.log("Locale data received in preload:", localeData);
+            callback(localeData);
+        });
+    }
+});
